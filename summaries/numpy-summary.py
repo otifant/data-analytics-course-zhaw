871642d20f41e16352f9c0878dd1ec275a4d4e29
Samuel <samuel.grimm@hotmail.ch>
2023-03-14 20:39:05 +0100

    finished lesson 2

---
+++ summaries/numpy-summary.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+np.linspace(-10,10,21) # Gets 21 values between -10 and 10:
+# array([-10.,  -9.,  -8.,  -7.,  -6.,  -5.,  -4.,  -3.,  -2.,  -1.,   0.,
+#         1.,   2.,   3.,   4.,   5.,   6.,   7.,   8.,   9.,  10.])
+
+a,b,c = 1,1,1
+np.roots([a,b,c]) # Mitternachtsformel
+
